b3d96f1221f3ccd177f115886ff6eb6f1857d218
QuangTrongLeo <trongphamtg05@gmail.com>
2026-08-18 11:34:24 +0700

    chore: initialize smart-job project

---
+++ be-smart-job/src/main/java/be_smart_job/BeSmartJobApplication.java
@@ -0,0 +1,13 @@
+package be_smart_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSmartJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSmartJobApplication.class, args);
+	}
+
+}
---
+++ be-smart-job/src/test/java/be_smart_job/BeSmartJobApplicationTests.java
@@ -0,0 +1,13 @@
+package be_smart_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSmartJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
